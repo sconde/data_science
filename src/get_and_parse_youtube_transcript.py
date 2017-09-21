798a00bd6470308a81510bfe7b55692463aa2c91
Sidafa Conde <sconde@umassd.edu>
2017-09-21 19:13:25 -0400

    simple script to scrape the transcript from youtube video and copy the text to the clipbord

---
+++ src/get_and_parse_youtube_transcript.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import mechanize
+from bs4 import BeautifulSoup
+import pyperclip # for copying the file to the clipbord (control+c)
+
+br = mechanize.Browser(factory=mechanize.RobustFactory())
+br.set_handle_robots(False)
+video_address = sys.argv[1] if len(sys.argv) == 2 else 'BHRQEFsAqpc'
+youtube_link = 'http://video.google.com/timedtext?lang=en&v='
+xml_link = '%s%s' %(youtube_link, video_address)
+
+webpage = br.open(xml_link).read()
+soupPage = BeautifulSoup(webpage, "xml")
+soup = soupPage.find('transcript').findAll('text')
+text_list = [x.string.encode('ascii','ignore') for x in soup]
+print text_list
+strin_all = ""
+pyperclip.copy("\n".join(text_list))
+
